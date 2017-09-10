fa6b05120f32c9e411ebc707e79139fbca1ec01d
cpalomino <laignelet2@gmail.com>
2017-09-10 13:24:54 -0700

    Finished

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -5,6 +5,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 import _ from 'lodash';
+import {Link} from 'react-router-dom';
 
 
 class PostsIndex extends Component {
@@ -16,7 +17,7 @@ class PostsIndex extends Component {
     renderPosts(){
        return  _.map(this.props.posts, post=> {
            return (<li className="list-group-item" key={post.id}>
-               {post.title}
+               <Link to={`/posts/${post.id}`}>{post.title}</Link>
            </li>);
        })
     }
@@ -25,7 +26,12 @@ class PostsIndex extends Component {
 
         return (
             <div>
-            <h3>Posts index</h3>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
+            <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
                 </ul>
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,17 @@
 /**
  * Created by cpalomino on 9/7/2017.
  */
-import {FETCH_POSTS} from '../actions/index';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions/index';
 import _ from 'lodash'
 
 export default function(state = {}, action){
     switch (action.type){
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         case FETCH_POSTS :
             return  _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST :
+            return  {...state, [action.payload.data.id] : action.payload.data };
         default :
             return state;
     }
